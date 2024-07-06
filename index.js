afb2ec716d8e635be616f55e7a4a284693931975
Ivy Nyambura <ikagam018@gmail.com>
2024-07-06 23:03:19 +0300

    did DOM

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here!
+function NoLonger(){
+    const body = document.querySelector("body");
+    const secondChild = body.querySelector("main")
+    body.removeChild(secondChild);
+
+}
+NoLonger()
+
+const newHeader = document.createElement("h1")
+document.body.appendChild(newHeader)
+
+function changeId(){
+    newHeader.id = "victory"
+
+
+}
+changeId()
+
+function textInside(){
+    const newHeader = document.getElementById("victory")
+    newHeader.innerHTML = "Ivy is the champion"
+}
+textInside()
